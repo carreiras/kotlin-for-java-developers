667bfe17a3ab25741edff2b77bc9f4619ff5424d
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-07-09 13:54:52 -0300

    Iniciando o projeto - Criando classe Main

---
+++ src/main/java/com/ewecarreira/Main.java
@@ -0,0 +1,8 @@
+package com.ewecarreira;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/main/kotlin/com/ewecarreira/Main.kt
@@ -0,0 +1,5 @@
+package com.ewecarreira
+
+fun main() {
+    println("Hello, World!")
+}
